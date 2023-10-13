5c6bb23dff4b417469b010606508c028fbaccf72
Tony Wang <Tony94042@gmail.com>
2023-10-13 19:20:50 +1100

    Seperated the error message into a seperate file

---
+++ src/app/error-message/error-message.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-error-message',
+  templateUrl: './error-message.component.html',
+  styleUrls: ['./error-message.component.css']
+})
+export class ErrorMessageComponent {
+  @Input() control?: FormControl;
+  constructor(){
+    console.log(this.control)
+  }
+}
